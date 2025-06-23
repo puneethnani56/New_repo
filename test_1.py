3a69921c4bbc69319c12267d318e1eba5620c200
puneethnani <puneethnani56@gmail.com>
2025-06-23 10:48:55 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -3,4 +3,5 @@ from note import list_sum  # replace `your_module` with the actual file name (wi
 def test_custom_sum():
     a = list(range(1, 100, 2))  # [1, 3, 5, ..., 99]
     expected = list_sum(a)  # built-in sum for validation
-    assert test_custom_sum() == expected
+    return expected
+assert test_custom_sum() == sum(list_sum)
