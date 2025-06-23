5ef068e9576fc378fd9b88a466595a59d4e24a70
puneethnani <puneethnani56@gmail.com>
2025-06-23 10:49:47 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -4,4 +4,4 @@ def test_custom_sum():
     a = list(range(1, 100, 2))  # [1, 3, 5, ..., 99]
     expected = list_sum(a)  # built-in sum for validation
     return expected
-assert test_custom_sum() == sum(list_sum)
+assert test_custom_sum() == sum(list_sum())
